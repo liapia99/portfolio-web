14ac5155af6f525f1ce8594fbfc18cb4e64d5efa
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-20 08:24:03 -0500

    Add files via upload

---
+++ about.js
@@ -0,0 +1,18 @@
+import '../App.css';
+
+
+function About() {
+    return (
+        <div className="aboutpage">
+            <p>
+                I started coding in 2021 and fell in love with how I could bring by web designs to life . Since starting university, I have become more curious and have explored many routes in technology including drones, PCB designing, robotics using artificial intelligence, OpenCV, LiDAR scanning, and augmented reality. This journey has not only expanded my skills but has also fueled my passion for creating innovative solutions in our evolving world.
+
+                These days I have been focusing on researching machine learning in dermatology and the possibilities of expanding the image datasets to assist in patient care plans.
+
+                When I am not studying or coding, I’m most likely listening to music, reading, napping with my dog or creating digital illustrations.
+            </p>
+        </div>
+    );
+}
+
+export default About;
---
+++ contact.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+
+function Contact() {
+    return (
+        <div className="contactpage">
+            <p>
+                You can reach me at piascikj2@mail.sacredheart.edu!
+                If you want to see what I am currently up to, check out my Notion Blog called 'brain dumps'.
+            </p>
+        </div>
+    );
+}
+
+export default Contact;
